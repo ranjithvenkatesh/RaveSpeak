28749d2c9b07a7dc8d96456c0cef23433111199b
Ranjith Venkatesh <Ranjith.Venkatesh@mossandlichens.com>
2019-04-29 08:07:45 +0200

    Unit Tests

---
+++ RaveSpeak.UnitTests/Helpers/TextStatisticsHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaveSpeak.Helpers;
+
+namespace RaveSpeak.UnitTests.Helpers
+{
+    [TestClass]
+    public class TextStatisticsHelperTests
+    {
+        [TestMethod]
+        public void FleschKincaidGradeLevelTest()
+        {
+            // Arrange and Act
+            var textStatisticsHelper = new TextStatisticsHelper("the quick brown fox jumps over the lazy dog");
+
+            // Assert
+            Assert.AreEqual(2.3, textStatisticsHelper.FleschKincaidGradeLevel);
+        }
+    }
+}
